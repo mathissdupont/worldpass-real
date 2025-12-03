888e5a07ceb71a177418f3c95375fea176101182
Samet Unsal <sametutku16@gmail.com>
2025-12-03 21:07:06 +0300

    Cleanup: Remove unnecessary closing div in IssuerAPIWebhooks component

--- web/src/pages/issuer/console/APIWebhooks.jsx
+++ web/src/pages/issuer/console/APIWebhooks.jsx
@@ -402,8 +402,7 @@ export default function IssuerAPIWebhooks() {
             onRowClick={(row) => openEditWebhookModal(row)}
           />
         </div>
-      </div>
-
+      
       {/* Webhook Create/Edit Modal */}
       {showWebhookModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
